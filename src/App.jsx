565705141c53d7866f50cebf47a9c1f9f36b655b
Driss1-jabri <jabri.dri.fst@uhp.ac.ma>
2024-05-12 13:25:33 +0100

    login

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Analyse } from "./components/Analyse";
 import Home from "./components/Home";
 import { General } from "./components/General";
 import { Consultation } from "./components/Consultation";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
+import { ConfirmationCode } from "./components/ConfirmationCode";
 
 export default function App() {
   return (
@@ -13,6 +16,9 @@ export default function App() {
          <Route path="/" element={<General />} />
          <Route path="/analyse" element={<Analyse />} />
          <Route path="/consultation" element={<Consultation />} />
+         <Route path="/register" element={<Register />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/confirmation" element={<ConfirmationCode />} />
       </Routes>
     </BrowserRouter>
     
